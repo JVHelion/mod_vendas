6346e3612dd10118cef319dfac0cbfa6e0af81d1
joao.helion <joao.helion@soberanium.com>
2025-06-26 20:24:06 -0300

    fix: route hello world

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::group(['prefix' => 'mod_vendas'], function () {
     Route::get('buscar_produtos', [ProdutoController::class, 'buscarProdutos']);
     Route::post('criar_pedido', [PedidoController::class, 'inserirPedidos']);
     Route::post('criar_pagamento', [PagamentoController::class, 'inserirPagamentos']);
-    Route::get('hello-world', function () {
-        return response()->file(public_path('angular/hello-world-spa/index.html'));
-    });
+    Route::get('/hello-world/{any}', function () {
+    return response()->file(public_path('angular/hello-world-spa/index.html'));
+})->where('any', '.*');
 });
